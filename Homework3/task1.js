579d1f2bdcb6448db7ef2b08d43634c8862ac632
zakariannn <72627891+zakariannn@users.noreply.github.com>
2021-09-01 03:20:55 +0400

    Update task1.js

--- Homework3/task1.js
+++ Homework3/task1.js
@@ -1,1 +1,9 @@
-
+function flatten(ary) {
+  return ary.reduce(function(a, b) {
+    if (Array.isArray(b)) {
+      return a.concat(flatten(b))
+    }
+    return a.concat(b)
+  }, [])
+}
+console.log(flatten([14, [1, [[[3, []]], 1], 0]]));
